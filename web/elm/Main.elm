503966ad1847c0083c6535cae66e58f766be1399
Ricardo García Vega <bigardone@gmail.com>
2016-08-15 20:35:13 +0200

    basic main elm structure

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,8 +1,92 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.App as App
 
 
-main : Html a
+-- model
+
+
+type alias Contact =
+    { id : Int
+    , first_name : String
+    , last_name : String
+    , gender : Int
+    , birth_date : String
+    , location : String
+    , phone_number : String
+    , email : String
+    , headline : String
+    , picture : String
+    }
+
+
+type alias Model =
+    { entries : List Contact
+    , page_number : Int
+    , total_entries : Int
+    , total_pages : Int
+    , search : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { entries = []
+    , page_number = 1
+    , total_entries = 0
+    , total_pages = 0
+    , search = ""
+    }
+
+
+
+-- messages
+
+
+type Msg
+    = Page Int
+    | Search String
+
+
+
+-- update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Page pageNumber ->
+            model
+
+        Search search ->
+            model
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        [ text "" ]
+
+
+main : Program Never
 main =
-    text "Hello, World!"
+    App.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
