1bec45a91dd909a3f3b338c20a27741ef7f3068e
Ricardo García Vega <bigardone@gmail.com>
2016-08-16 07:56:57 +0200

    minor refactoring

--- web/controllers/contact_controller.ex
+++ web/controllers/contact_controller.ex
@@ -1,13 +1,13 @@
 defmodule PhoenixAndElm.ContactController do
   use PhoenixAndElm.Web, :controller
 
-  alias PhoenixAndElm.{Repo, Contact}
+  alias PhoenixAndElm.Contact
 
   def index(conn, params) do
     page = Contact
       |> order_by(:first_name)
       |> Repo.paginate(params)
 
-    render(conn, "index.json", page: page)
+    render conn, page: page
   end
 end
