f630be081f244585454c17e17c7e28d6f9481b2e
Ricardo García Vega <bigardone@gmail.com>
2016-08-17 17:37:45 +0200

    added full text search for contacts

--- web/controllers/contact_controller.ex
+++ web/controllers/contact_controller.ex
@@ -4,10 +4,13 @@ defmodule PhoenixAndElm.ContactController do
   alias PhoenixAndElm.Contact
 
   def index(conn, params) do
+    search = Map.get(params, "search") || ""
+
     page = Contact
+      |> Contact.search(search)
       |> order_by(:first_name)
       |> Repo.paginate(params)
 
-    render conn, page: page
+    render conn, page: page, search: search
   end
 end
--- web/views/contact_view.ex
+++ web/views/contact_view.ex
@@ -1,5 +1,5 @@
 defmodule PhoenixAndElm.ContactView do
   use PhoenixAndElm.Web, :view
 
-  def render("index.json", %{page: page}), do: page
+  def render("index.json", %{page: page, search: search}), do: Map.put(page, :search, search)
 end
