b4c5f58975ad1d64e615dc8195eea2a6c1fa5def
Ricardo García Vega <bigardone@gmail.com>
2017-02-03 07:06:22 +0100

    deps update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixAndElm.Mixfile do
   def project do
     [app: :phoenix_and_elm,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
