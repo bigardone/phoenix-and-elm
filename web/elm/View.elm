c865f51e403e77a1f765cd66ad042efa8a5405ee
Ricardo García Vega <bigardone@gmail.com>
2017-03-07 08:09:33 +0100

    render view depending on current route

--- web/elm/View.elm
+++ web/elm/View.elm
@@ -3,8 +3,10 @@ module View exposing (..)
 import ContactList.View exposing (indexView)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Messages exposing (..)
 import Model exposing (..)
+import Routing exposing (Route(..))
 
 
 view : Model -> Html Msg
@@ -13,7 +15,7 @@ view model =
         []
         [ headerView
         , div []
-            [ indexView model ]
+            [ page model ]
         ]
 
 
@@ -24,4 +26,39 @@ headerView =
         [ h1
             []
             [ text "Phoenix and Elm: A real use case" ]
+        ]
+
+
+page : Model -> Html Msg
+page model =
+    case model.route of
+        HomeIndexRoute ->
+            indexView model
+
+        NotFoundRoute ->
+            notFoundView
+
+
+notFoundView : Html Msg
+notFoundView =
+    div
+        [ id "error_index" ]
+        [ div
+            [ class "warning" ]
+            [ span
+                [ class "fa-stack" ]
+                [ i
+                    [ class "fa fa-meh-o fa-stack-2x" ]
+                    []
+                ]
+            , h4
+                []
+                [ text "404" ]
+            , p
+                []
+                [ text "The page you're looking for can't be found." ]
+            , a
+                [ onClick <| NavigateTo HomeIndexRoute ]
+                [ text "Back to home" ]
+            ]
         ]
